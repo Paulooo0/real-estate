ba6a56e32ada9bfa40c70ed8597bae70b45a2be2
Paulooo0 <paulohenrique.n.s00@gmail.com>
2024-01-10 19:51:22 -0300

    UPDATE: ObjectOperations are now generic; UPDATE: .env.example new environment variables; ADD: (last commit) S3Config

--- real-state/src/main/java/br/com/realstate/infra/S3/ObjectOperations.java
+++ real-state/src/main/java/br/com/realstate/infra/S3/ObjectOperations.java
@@ -2,54 +2,26 @@ package br.com.realstate.infra.S3;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
-
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ListObjectsV2Result;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 import br.com.realstate.config.S3Config;
 
 public class ObjectOperations extends S3Config {
-    @Value("${AWS_ACCESS_KEY}")
-    private String accessKey;
-
-    @Value("${AWS_SECRET_KEY}")
-    private String secretKey;
-
-    @Value("${IMAGES_BUCKET}")
-    private String imagesBucket;
-
-    private final AmazonS3 s3;
-
-    public ObjectOperations() {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-
-        s3 = AmazonS3ClientBuilder.standard()
-        .withRegion(Regions.SA_EAST_1)
-        .withCredentials(new AWSStaticCredentialsProvider(credentials))
-        .build();
-    }
-
-    public List<String> ListObjects() {
-        ListObjectsV2Result result = s3.listObjectsV2(imagesBucket);
+    public List<String> ListObjects(String bucketName) {
+        ListObjectsV2Result result = s3.listObjectsV2(bucketName);
         List<S3ObjectSummary> objects = result.getObjectSummaries();
         return objects.stream().map(S3ObjectSummary::getKey).toList();
     }
 
-    public void deleteObject(String key) {
-        if (!s3.doesObjectExist(imagesBucket, key)) {
+    public void deleteObject(String bucketName, String key) {
+        if (!s3.doesObjectExist(bucketName, key)) {
             return;
         }
-        s3.deleteObject(imagesBucket, key);
+        s3.deleteObject(bucketName, key);
     }
 
-    public void sendObject(String key, String content) {
-        s3.putObject(imagesBucket, key, content);
+    public void sendObject(String bucketName, String key, String content) {
+        s3.putObject(bucketName, key, content);
     }
 }
