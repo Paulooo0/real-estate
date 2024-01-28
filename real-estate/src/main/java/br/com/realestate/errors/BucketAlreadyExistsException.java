927bbae9cff7e44fe804a64885f4388fde62cba5
Paulooo0 <paulohenrique.n.s00@gmail.com>
2024-01-28 13:34:07 -0300

    REMOVE: S3 database and its dependencies

--- real-estate/src/main/java/br/com/realestate/errors/BucketAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package br.com.realestate.errors;
-
-public class BucketAlreadyExistsException extends Exception {
-
-    public BucketAlreadyExistsException(String bucketName) {
-        super(String.format("Bucket %s already exists", bucketName));
-    }
-}
